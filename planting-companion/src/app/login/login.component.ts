1a956a5a30f5208a058638607650abe13d7e22ed
jdmartens <jeremydmartens@gmail.com>
2025-03-21 16:23:13 -0500

    add reactive login form with validation and error handling

--- planting-companion/src/app/login/login.component.ts
+++ planting-companion/src/app/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../core/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
 
+  errorMessage = '';
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email!, password!).subscribe({
+        next: (response) => {
+          this.authService.handleLoginSuccess(response.access_token);
+        },
+        error: (err) => {
+          this.errorMessage = err;
+        }
+      });
+    }
+  }
 }
