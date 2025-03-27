be1c9e83d4580c604d2a9daffe1cae4ee237e067
jdmartens <jeremydmartens@gmail.com>
2025-03-27 13:38:32 -0500

    add route for UserMgmtComponent to enable admin user management

--- planting-companion/src/app/app.routes.ts
+++ planting-companion/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { UserMgmtComponent } from './user-mgmt/user-mgmt.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: UserMgmtComponent }
 ];
