9f1095c6a29ea0c2f30b86aa14d93893ec6f0fc9
jdmartens <jeremydmartens@gmail.com>
2025-03-29 20:51:21 -0500

    add dashboard route to application

--- planting-companion/src/app/app.routes.ts
+++ planting-companion/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserMgmtComponent } from './user-mgmt/user-mgmt.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'dash', component: DashboardComponent },
   { path: 'admin', component: UserMgmtComponent }
 ];
