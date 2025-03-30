137f98de02a13fe7f477a0449a8eba5835f47e31
jdmartens <jeremydmartens@gmail.com>
2025-03-30 11:37:35 -0500

    add logout route to application

--- planting-companion/src/app/app.routes.ts
+++ planting-companion/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserMgmtComponent } from './user-mgmt/user-mgmt.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LogoutComponent } from './logout/logout.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
   { path: 'dash', component: DashboardComponent },
   { path: 'admin', component: UserMgmtComponent }
 ];
