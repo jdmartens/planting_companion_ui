6eaed280d3f3334081d35dfbac440fed108a13fe
jdmartens <jeremydmartens@gmail.com>
2025-03-31 16:24:32 -0500

    add theme switcher component with HTML, CSS, and tests

---
+++ planting-companion/src/app/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ThemeService } from '../core/theme.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-theme-switcher',
+  imports: [CommonModule], 
+  templateUrl: './theme-switcher.component.html'
+  
+})
+export class ThemeSwitcherComponent {
+  constructor(public themeService: ThemeService) {}
+
+  toggleTheme() {
+    // this.themeService.toggleTheme();
+  }
+}
