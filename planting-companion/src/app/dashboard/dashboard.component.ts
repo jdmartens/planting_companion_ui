08b475cf820c0481c64a4040e672242dee1c3b98
jdmartens <jeremydmartens@gmail.com>
2025-03-31 16:25:20 -0500

    add theme switcher component to dashboard

--- planting-companion/src/app/dashboard/dashboard.component.ts
+++ planting-companion/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { UserStateService } from '../store/user-state.service';
+import { ThemeSwitcherComponent } from '../theme-switcher/theme-switcher.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [ThemeSwitcherComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
