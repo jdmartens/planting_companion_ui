c47452c92ebc5470ec9f40e16eed9b142bf936d0
jdmartens <jeremydmartens@gmail.com>
2025-04-09 20:42:48 -0500

    add error handling to password update in current user service

--- planting-companion/src/app/core/current-user.service.ts
+++ planting-companion/src/app/core/current-user.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -11,7 +11,11 @@ export class CurrentUserService {
   private readonly apiUrl = environment.apiUrl;
 
   updateMyPassword(currentPassword: string, newPassword: string): Observable<void> {
-    return this.http.patch<void>(`${this.apiUrl}/me/password`, { currentPassword, newPassword });
+    return this.http.patch<void>(`${this.apiUrl}/me/password`, { currentPassword, newPassword }).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error);
+      })
+    );
   }
 
   getMe(): Observable<any> {
