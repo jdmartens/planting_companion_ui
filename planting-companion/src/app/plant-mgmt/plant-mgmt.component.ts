e69785d33f9ec1e7060edaa5283f5d2dacf60102
jdmartens <jeremydmartens@gmail.com>
2025-04-16 19:56:01 -0500

    fetch plants from the backend

--- planting-companion/src/app/plant-mgmt/plant-mgmt.component.ts
+++ planting-companion/src/app/plant-mgmt/plant-mgmt.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlantService, Plant } from '../core/plant.service';
 
 @Component({
   selector: 'app-plant-mgmt',
-  imports: [],
   templateUrl: './plant-mgmt.component.html',
   styleUrl: './plant-mgmt.component.css'
 })
-export class PlantMgmtComponent {
+export class PlantMgmtComponent implements OnInit {
+  plants: Plant[] = [];
+  loading: boolean = false;
 
+  constructor(private plantService: PlantService) {}
+
+  ngOnInit(): void {
+    this.fetchPlants();
+  }
+
+  fetchPlants(): void {
+    this.loading = true;
+    this.plantService.getPlants().subscribe({
+      next: (plants) => {
+        this.plants = plants;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Failed to fetch plants:', error);
+        this.loading = false;
+      }
+    });
+  }
 }
