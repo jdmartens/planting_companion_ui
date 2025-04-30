e14248fe80ec18f6f8583242f6b953b8dcc85047
jdmartens <jeremydmartens@gmail.com>
2025-04-30 08:18:08 -0500

    theme switcher now located under user settings

--- planting-companion/src/app/dashboard/dashboard.component.ts
+++ planting-companion/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { UserStateService } from '../store/user-state.service';
-import { ThemeSwitcherComponent } from '../theme-switcher/theme-switcher.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ThemeSwitcherComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  userStateService = inject(UserStateService); // Inject the UserStateService
+  userStateService = inject(UserStateService);
 }
