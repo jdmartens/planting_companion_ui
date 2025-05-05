c4c33b471bed50a80692cf15e626057bee7e24b5
jdmartens <jeremydmartens@gmail.com>
2025-05-04 21:56:46 -0500

    add plant details component and update plant management links

---
+++ planting-companion/src/app/plant-details/plant-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plant-details',
+  imports: [],
+  templateUrl: './plant-details.component.html',
+  styleUrl: './plant-details.component.css'
+})
+export class PlantDetailsComponent {
+
+}
