7308eb6d3fc64f2db37fc046002353e486219c15
jdmartens <jeremydmartens@gmail.com>
2025-05-04 21:57:00 -0500

    add route for plant details component

--- planting-companion/src/app/app.routes.ts
+++ planting-companion/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LogoutComponent } from './logout/logout.component';
 import { UserSettingsComponent } from './user-settings/user-settings.component';
 import { PlantMgmtComponent } from './plant-mgmt/plant-mgmt.component';
+import { PlantDetailsComponent } from './plant-details/plant-details.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'dash', component: DashboardComponent },
   { path: 'settings', component: UserSettingsComponent },
   { path: 'admin', component: UserMgmtComponent },
-  { path: 'plants', component: PlantMgmtComponent }
+  { path: 'plants', component: PlantMgmtComponent },
+  { path: 'plants/:uid', component: PlantDetailsComponent },
 ];
